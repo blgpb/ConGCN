a618209558f5bcbd073a5a3ed6fff49ad2b0d76a
blgpb <windu.yu@qq.com>
2024-06-07 00:03:37 +0800

    init
    init

--- post_process.m
+++ post_process.m
@@ -1,9 +1,7 @@
-% 2021年10月22日10:02:42
-% 将可视化的pred_mat的边缘美化
 load('data/paviaU/paviaU_gt.mat');
 load('pred_mat_1.mat');
 pos = find(paviaU_gt==0);
 pred_mat(pos)=0;
-imshow(label2rgb(pred_mat)); % predicted result
+imshow(label2rgb(pred_mat));
 figure(2)
-imshow(label2rgb(paviaU_gt)); % groundtruth
+imshow(label2rgb(paviaU_gt));
